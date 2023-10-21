234d43e73c141238469ed9de718591115646b1ef
Alice <admsalice>
2023-10-21 22:00:34 +0300

    програм068

--- Program.cs
+++ Program.cs
@@ -3,28 +3,28 @@
 //Даны два неотрицательных числа m и n.
 
 Console.Write("Введите число M: ");
-int m = Convert.ToInt32(Console.ReadLine());
+int M = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 
-AkkermanFunction(m,n);
-void AkkermanFunction(int m, int n)
+AkkermanFunction(M,N);
+void AkkermanFunction(int M, int N)
 {
-    Console.Write(Akkerman(m, n)); 
+    Console.Write(Akkerman(M, N)); 
 }
-int Akkerman(int m, int n)
+int Akkerman(int M, int N)
 {
-    if (m == 0)
+    if (M == 0)
     {
-        return n + 1;
+        return N + 1;
     }
-    else if (n == 0 && m > 0)
+    else if (N == 0 && M > 0)
     {
-        return Akkerman(m - 1, 1);
+        return Akkerman(M - 1, 1);
     }
     else
     {
-        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+        return (Akkerman(M - 1, Akkerman(M, N - 1)));
     }
 }
